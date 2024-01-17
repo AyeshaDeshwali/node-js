0a8d773740275a636e9b2d3ed5bc2b6936e212c1
Ayesha Deshwali <139254189+AyeshaDeshwali@users.noreply.github.com>
2024-01-17 17:15:09 +0530

    Updates

--- step-by-step/36Post-API-insert data.js
+++ step-by-step/36Post-API-insert data.js
@@ -1,8 +1,10 @@
+// Post API insert data in MongoDB
+
 const express = require("express");
 const dbConnect = require("./mongodb");
 const app = express();
-
 const PORT = 5000;
+
 app.use(express.json());
 app.get("/", async (req, res) => {
   let data = await dbConnect();
@@ -11,14 +13,10 @@ app.get("/", async (req, res) => {
   res.send(data);
 });
 
-// app.post("/", (req, res) => {
-//   console.log(req.body);
-//   res.send(req.body);
-// });
-
 app.post("/", async (req, res) => {
-  let data = await db.dbConnect();
-  let result = await data.insert(req.body);
+  console.log(req.body);
+  let data = await dbConnect();
+  let result = await data.insertMany(req.body);
   res.send(result);
 });
 
