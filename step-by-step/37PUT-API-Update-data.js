825130d05e9331e9945c963088563f0fc8d1190a
Ayesha Deshwali <139254189+AyeshaDeshwali@users.noreply.github.com>
2024-01-20 16:37:10 +0530

    Updates

--- step-by-step/37PUT-API-Update-data.js
+++ step-by-step/37PUT-API-Update-data.js
@@ -4,16 +4,70 @@ let app = express();
 let port = 3000;
 app.use(express.json());
 app.put("/", async (req, res) => {
-  try {
-    let data = await dbConnect();
-    let result = await data.updateOne({ name: "husain" }, { $set: req.body });
-    console.log(result);
-    res.send({ result: "update" });
-  } catch (error) {
-    console.error("Error updating document:", error);
-    res.status(500).send({ result: "update failed" });
-  }
+let data = await dbConnect();
+let result = await data.updateOne(
+  { name: "Abubakar" },
+  { $set: { mobile: 4234645723 } }
+);
+  res.send(result);
 });
 app.listen(port, () => {
-  console.log(`server is running ${port}`);
+  console.log(`server is running on ${port}`);
 });
+
+
+
+
+let dbConnect = require("./mongodb");
+let express = require("express");
+let app = express();
+let port = 3000;
+app.use(express.json());
+app.put("/", async (req, res) => {
+  let data = await dbConnect();
+  data = await data.updateOne({ name: "Abubakar" }, { $set: req.body });
+  res.send({ result: "update" });
+});
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
+
+
+
+
+
+let dbConnect = require("./mongodb");
+let express = require("express");
+let app = express();
+let port = 3000;
+app.use(express.json());
+app.put("/", async (req, res) => {
+  let data = await dbConnect();
+  data = await data.updateOne({ name: req.body.name }, { $set: req.body });
+  res.send({ result: "update" });
+});
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
+
+
+
+let dbConnect = require("./mongodb");
+let express = require("express");
+let app = express();
+let port = 3000;
+app.use(express.json());
+app.put("/:name", async (req, res) => {
+let data = await dbConnect();
+let result = await data.updateOne(
+  { name: req.params.name },
+  { $set: req.body }
+); 
+  res.send({ result: "update" });
+});
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
+
+
+
