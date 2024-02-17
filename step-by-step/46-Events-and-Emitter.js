46147a51123fd34bc7445b83bae9a7d92da66fb7
Ayesha Deshwali <139254189+AyeshaDeshwali@users.noreply.github.com>
2024-02-17 09:45:19 +0530

    Updated in this file

--- step-by-step/46-Events-and-Emitter.js
+++ step-by-step/46-Events-and-Emitter.js
@@ -7,6 +7,10 @@ const hi = () => {
 myEmitter.addListener("hello", hi);
 myEmitter.removeListener("hello", hi);
 
+
+
+
+
 const EventEmitter = require("events");
 class MyEmitter extends EventEmitter {}
 const myEmitter = new MyEmitter();
@@ -20,3 +24,28 @@ myEmitter.removeListener("hello", () => {
   console.log("Hello, remove!");
 });
 myEmitter.emit("hello");
+
+
+
+const express = require("express");
+const EventEmitter = require("events");
+let app = express();
+const events = new EventEmitter();
+let count = 0;
+events.on("countAPI", () => {
+  count++;
+  console.log("event called ", count);
+});
+app.get("/", (req, res) => {
+  res.send("hello home api");
+  events.emit("countAPI");
+});
+app.get("/search", (req, res) => {
+  res.send("hello search api");
+  events.emit("countAPI");
+});
+app.get("/update", (req, res) => {
+  res.send("hello update api");
+  events.emit("countAPI");
+});
+app.listen(2030);
