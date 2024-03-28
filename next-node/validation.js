78b2519bc4cdb8004352400523e3b6708eb9dee3
Ayesha Deshwali <139254189+AyeshaDeshwali@users.noreply.github.com>
2024-03-28 23:27:47 +0530

    Update validation.js

--- next-node/validation.js
+++ next-node/validation.js
@@ -1,18 +1,22 @@
-db.createCollection("nonfiction",{
-  validator:{
-    $jsonSchema:{
-      required:['name','price'],
-      properties:{
-        name:{
-          bsonType:'string',
-          description:'must be a string and required'
+db.createCollection("nonfiction", {
+  validator: {
+    $jsonSchema: {
+      required: ['name', 'price', 'author'], 
+      properties: {
+        name: {
+          bsonType: 'string',
+          description: 'must be a string and required'
         },
-        price:{
-          bsonType:'number',
-          description:'must be number and required'
+        price: {
+          bsonType: 'number',
+          description: 'must be number and required'
+        },
+        author: {
+          bsonType: 'string',
+          description: 'must be a string and required'
         }
       }
     }
   },
-  validationAction:'error'
+  validationAction: 'error'
 })
