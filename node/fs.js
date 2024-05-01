9b756c68d95da678b3d6a4ff511aeb879af512b1
Ayesha Deshwali <139254189+AyeshaDeshwali@users.noreply.github.com>
2024-05-01 18:47:45 +0530

    Update fs.js

--- node/fs.js
+++ node/fs.js
@@ -1,24 +1,24 @@
 let fs = require("fs");
 fs.writeFileSync("index.js", "console.log('hello world')");
-// fs.unlinkSync("index.js");
+fs.unlinkSync("index.js");
 
-// let fs = require("fs");
-// let path = require("path");
-// let dirPath = path.join(__dirname, "crud");
-// fs.writeFileSync(`${dirPath}/index.html`, "hello world");
+let fs = require("fs");
+let path = require("path");
+let dirPath = path.join(__dirname, "crud");
+fs.writeFileSync(`${dirPath}/index.html`, "hello world");
 
-// let fs = require("fs");
-// let path = require("path");
-// let dirPath = path.join(__dirname, "crud");
-// for (let i = 1; i <= 5; i++) {
-//   fs.writeFileSync(`${dirPath}/index${i}.html`, "hello world");
-// }
+let fs = require("fs");
+let path = require("path");
+let dirPath = path.join(__dirname, "crud");
+for (let i = 1; i <= 5; i++) {
+  fs.writeFileSync(`${dirPath}/index${i}.html`, "hello world");
+}
 
-// let fs = require("fs");
-// let input = process.argv;
-// if (input[2] === "add") {
-//   fs.writeFileSync(input[3], input[4]);
-// } else if (input[2] === "remove") {
-//   fs.unlinkSync(input[3]);
-// } else {
-//   console.log("invalid file");
+let fs = require("fs");
+let input = process.argv;
+if (input[2] === "add") {
+  fs.writeFileSync(input[3], input[4]);
+} else if (input[2] === "remove") {
+  fs.unlinkSync(input[3]);
+} else {
+  console.log("invalid file");
