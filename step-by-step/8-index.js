a6a9b028c5e6dcf1ad5d418b53d3a38a125b0989
ayeshaDeshwali <ayeshadeshwali@gmail.com>
2024-12-10 08:01:14 +0530

    add files

--- step-by-step/8-index.js
+++
@@ -1,7 +0,0 @@
-// 8-Package.json-file
-
-// Package.json-file  ye file project ki details rkte h coding se related jase ki project ka version kya h abhi, uska name kya h, uski git repository kya h, uske ander kon-kon si commands use ho rhi h, use ke ander kon-kon se Package dal rke h, to ye sub Package.json-file ke ander mange hote h
-
-//  Node Package Manager(NPM) /// node ke Package ko un sub ko mange krta h jaise commnad,version etc.
-
-console.log("Package.json");
